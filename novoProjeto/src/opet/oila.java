2cd2ee5ecb15fdb17ee2a9e6d83ca011a53bfb8d
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.1.151>
2017-03-09 09:25:57 -0300

    ijijijij

--- novoProjeto/src/opet/oila.java
+++ novoProjeto/src/opet/oila.java
@@ -4,5 +4,8 @@ public class oila {
 public static void main(String[] args) {
 	System.out.println("Ola mundo lane");
 	System.out.println("Opeeeett");
+	System.out.println("Opeeeettao");
+	System.out.println("Opeeeettlalalal");
+	System.out.println("Opeeeetti");
 }
 }
