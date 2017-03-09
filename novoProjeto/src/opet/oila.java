a1c8d7244e395e992d0da156434ad1103d53ede8
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.1.151>
2017-03-09 08:53:52 -0300

    mudanças

--- novoProjeto/src/opet/oila.java
+++ novoProjeto/src/opet/oila.java
@@ -2,6 +2,7 @@ package opet;
 
 public class oila {
 public static void main(String[] args) {
-	System.out.println("Ola marilene");
+	System.out.println("Ola mundo lane");
+	System.out.println("Opeeeett");
 }
 }
